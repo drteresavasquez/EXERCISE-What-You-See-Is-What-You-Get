b0cb16a7bd86f8a43cd1b8e697297943d49ef04a
Dr. Teresa Vasquez <capewearingmama@Drs-MacBook-Pro.local>
2017-07-29 10:51:16 -0500

    made updates and changes

---
+++ wysiwyg.js
@@ -0,0 +1,186 @@
+console.log("wysiwyg.js, Yo!");
+
+//here is the object array of people who helped me learn to love myself, think, and DREAM!
+let famousFolks = [];
+
+	famousFolks.push({
+	  title: "Musician",
+	  name: "Prince",
+	  bio: "Prince Rogers Nelson (June 7, 1958 – April 21, 2016) was an American singer-songwriter, actor, multi-instrumentalist, philanthropist, dancer and record producer. He was a musical innovator who was known for his eclectic work, flamboyant stage presence, extravagant dress and makeup, and wide vocal range. His music integrates a wide variety of styles, including funk, rock, R&B, new wave, soul, psychedelia, and pop. He has sold over 100 million records worldwide, making him one of the best-selling artists of all time. He won seven Grammy Awards, an American Music Award, a Golden Globe Award, and an Academy Award for the film Purple Rain. He was inducted into the Rock and Roll Hall of Fame in 2004, his first year of eligibility.",
+	  image: "images/prince.png",
+	  lifespan: {
+	    birth: "June 7, 1958",
+	    death: "April 21, 2016"
+ 	 	}
+	});
+
+	famousFolks.push({
+	  title: "Author & Poet",
+	  name: "Langston Hughes",
+	  bio: "James Mercer Langston Hughes (February 1, 1902 – May 22, 1967) was an American poet, social activist, novelist, playwright, and columnist from Joplin, Missouri. He was one of the earliest innovators of the then-new literary art form called jazz poetry. Hughes is best known as a leader of the Harlem Renaissance in New York City. He famously wrote about the period that 'the negro was in vogue', which was later paraphrased as 'when Harlem was in vogue'.",
+	  image: "images/langstonhughes.png",
+	  lifespan: {
+	    birth: "February 1, 1902",
+	    death: "May 22, 1967"
+ 	 	}
+	});
+
+	famousFolks.push({
+	  title: "Author & Poet",
+	  name: "Ntosake Shange",
+	  bio: "Shange was born Paulette L. Williams[1] in Trenton, New Jersey,[5] to an upper-middle-class family. Her father, Paul T. Williams, was an Air Force surgeon, and her mother, Eloise Williams, was an educator and a psychiatric social worker. When she was aged eight, Shange's family moved to the racially segregated city of St. Louis. As a result of the Brown v. Board of Education court decision, Shange was bused to a white school where she endured racism and racist attacks.",
+	  image: "images/ntosakesange.png",
+	  lifespan: {
+	    birth: "October 18, 1948",
+	    death: "-"
+ 	 	}
+	});
+
+	famousFolks.push({
+	  title: "Artist & Musician",
+	  name: "Nina Simone",
+	  bio: "Simone was born Eunice Kathleen Waymon in North Carolina and raised in Tryon, North Carolina. The sixth of eight children in a poor family, she began playing piano at the age of three; the first song she learned was 'God Be With You, Till We Meet Again'. Demonstrating a talent with the instrument, she performed at her local church. But her concert debut, a classical recital, was given when she was 12. Simone later said that during this performance, her parents, who had taken seats in the front row, were forced to move to the back of the hall to make way for white people. She said that she refused to play until her parents were moved back to the front,[8][9] and that the incident contributed to her later involvement in the civil rights movement.",
+	  image: "images/ninasimone.png",
+	  lifespan: {
+	    birth: "February 21, 1933",
+	    death: "April 21, 2002"
+ 	 	}
+	});
+
+
+////////////////NEW
+	famousFolks.push({
+	  title: "Musician",
+	  name: "Maxwell",
+	  bio: "Maxwell was born in Brooklyn, New York, the son of a Haitian mother and a Puerto Rican father. His mother grew up in a devout Baptist household in Haiti.[4][5][6] Maxwell's father died in a plane crash when Maxwell was three years old.[7] The experience made him a deeply religious child, and during his childhood he sang in a Baptist church. Initially influenced by early-1980s urban R&B, Maxwell progressed rapidly, and by 1991 he was performing on the New York City club scene. Maxwell was able to gain access to a 24-track recording studio and started to record songs for a demo tape, which he circulated among his friends.[8] The demo engendered interest, and his official debut concert performance at Manhattan nightclub Nell's drew a crowd.[8] During the next two years, Maxwell wrote and recorded over three hundred songs and played frequently at small venues throughout New York City.[9] Maxwell's performances continued to draw interest and increase the buzz about him, and he was called 'the next Prince' by a writer from Vibe magazine who attended one of his shows.[8] After earning a considerable reputation, Maxwell signed a recording contract with Columbia Records in 1994. He adopted his middle name as a moniker out of respect for his family's privacy.[7]",
+	  image: "images/maxwell.png",
+	  lifespan: {
+	    birth: "May 23, 1973",
+	    death: "-"
+ 	 	}
+	});
+
+	famousFolks.push({
+	  title: "Musician",
+	  name: "Lauryn Hill",
+	  bio: "Lauryn Noelle Hill (born May 26, 1975) is an American singer, songwriter, rapper, record producer, and actress. She is best known for being a member of the Fugees and for her critically acclaimed solo album The Miseducation of Lauryn Hill, which won numerous awards and broke several sales records. Raised mostly in South Orange, New Jersey, Hill began singing with her music-oriented family during her childhood. She enjoyed success as an actress at an early age, with her older brother Graham Hill, appearing in a recurring role on the television soap opera As the World Turns and starring in the 1993 film Sister Act 2: Back in the Habit. In high school, Hill was approached by Pras Michel to start a band, which his friend, Wyclef Jean, soon joined. They renamed themselves the Fugees and released the albums Blunted on Reality (1994) and the Grammy Award-winning The Score (1996). In the latter record, which sold six million copies in the United States, Hill rose to prominence with her African-American and Caribbean music influences, her rapping and singing, and her rendition of the hit 'Killing Me Softly'. Hill's tumultuous romantic relationship with Jean led to the split of the band in 1997, after which she began to focus on solo projects.",
+	  image: "images/laurynhill.png",
+	  lifespan: {
+	    birth: "May 26, 1975",
+	    death: "-"
+ 	 	}
+	});
+
+
+	famousFolks.unshift({
+	  title: "TV Personality",
+	  name: "Oprah Winfrey",
+	  bio: "Oprah Winfrey (born Orpah Gail Winfrey; January 29, 1954) is an American media proprietor, talk show host, actress, producer, and philanthropist.[1] She is best known for her talk show The Oprah Winfrey Show, which was the highest-rated television program of its kind in history and was nationally syndicated from 1986 to 2011 in Chicago, Illinois.[5] Dubbed the 'Queen of All Media',[6] she has been ranked the richest African-American,[7] the greatest black philanthropist in American history,[8][9] and is currently North America's first and only multi-billionaire black person.[10] Several assessments rank her as the most influential woman in the world.[11][12] In 2013, she was awarded the Presidential Medal of Freedom by President Barack Obama[13] and honorary doctorate degrees from Duke and Harvard.[14][15]",
+	  image: "images/oprah.png",
+	  lifespan: {
+	    birth: "January 29, 1954",
+	    death: "-"
+ 	 	}
+	});
+
+	famousFolks.unshift({
+	  title: "Author, Speaker, Social Critic",
+	  name: "James Baldwin",
+	  bio: "James Arthur 'Jimmy' Baldwin (August 2, 1924 – December 1, 1987) was an American novelist, essayist, playwright, poet, and social critic. His essays, as collected in Notes of a Native Son (1955), explore palpable yet unspoken intricacies of racial, sexual, and class distinctions in Western societies, most notably in mid-20th-century America.[1] Some of Baldwin's essays are book-length, for instance The Fire Next Time (1963), No Name in the Street (1972), and The Devil Finds Work (1976). An unfinished manuscript, Remember This House, was expanded upon and adapted for cinema as the Academy Award-nominated documentary film I Am Not Your Negro. Baldwin's novels and plays fictionalize fundamental personal questions and dilemmas amid complex social and psychological pressures thwarting the equitable integration not only of African Americans, but also of gay and bisexual men, while depicting some internalized obstacles to such individuals' quests for acceptance. Such dynamics are prominent in Baldwin's second novel, Giovanni's Room, written in 1956, well before the gay liberation movement.[2]",
+	  image: "images/jamesbaldwin.png",
+	  lifespan: {
+	    birth: "August 2, 1924",
+	    death: "December 1, 1987"
+ 	 	}
+	});
+
+
+	famousFolks.push({
+	  title: "Educator & Activist",
+	  name: "Angela Davis",
+	  bio: "Angela Yvonne Davis (born January 26, 1944) is an American political activist, academic, and author. She emerged as a prominent counterculture activist and radical in the 1960s as a leader of the Communist Party USA, and had close relations with the Black Panther Party through her involvement in the Civil Rights Movement. Because she had purchased the firearms used in the crime, Davis was prosecuted for conspiracy involving the 1970 armed take-over of a Marin County, California, courtroom, in which four persons were killed. She was acquitted.[4] She was a professor (now retired) at the University of California, Santa Cruz, in its History of Consciousness Department and a former director of the university's Feminist Studies department.[5] Her research interests are feminism, African-American studies, critical theory, Marxism, popular music, social consciousness, and the philosophy and history of punishment and prisons. She co-founded Critical Resistance, an organization working to abolish the prison-industrial complex.",
+	  image: "images/angeladavis.png",
+	  lifespan: {
+	    birth: "January 26, 1944",
+	    death: "-"
+ 	 	}
+	});
+
+	famousFolks.unshift({
+	  title: "Activist",
+	  name: "Malcolm X",
+	  bio: "Malcolm X (1925–1965) was an African-American Muslim minister and human rights activist. To his admirers he was a courageous advocate for the rights of blacks, a man who indicted white America in the harshest terms for its crimes against black Americans; detractors accused him of preaching racism and violence. He has been called one of the greatest and most influential African Americans in history. Born Malcolm Little, Malcolm X was effectively orphaned early in life. His father was killed when he was six and his mother was placed in a mental hospital when he was thirteen, after which he lived in a series of foster homes. In 1946, at age twenty, he went to prison for larceny and breaking and entering. While in prison, he became a member of the Nation of Islam (NOI) and after his parole in 1952, quickly rose to become one of the organization's most influential leaders. He served as the public face of the controversial group for a dozen years. In his autobiography, Malcolm X wrote proudly of some of the social achievements the Nation made while he was a member, particularly its free drug rehabilitation program. The Nation promoted black supremacy, advocated the separation of black and white Americans, and rejected the civil rights movement for its emphasis on integration.",
+	  image: "images/malcolmx.png",
+	  lifespan: {
+	    birth: "May 19, 1925",
+	    death: "February 21, 1965"
+ 	 	}
+	});
+
+	famousFolks.push({
+	  title: "Author",
+	  name: "Alice Walker",
+	  bio: "Walker was born in Putnam County, Georgia,[4] the youngest of eight children, to African-American sharecroppers Willie Lee Walker and Minnie Lou Tallulah Grant. Her father, who was, in her words, 'wonderful at math but a terrible farmer,' earned only $300 ($4,000 in 2013 dollars) a year from sharecropping and dairy farming. Her mother supplemented the family income by working as a maid.[5] The family also had Native American ancestry, which Walker drew from in her writing and spirituality. Minnie Lou worked 11 hours a day for $17 per week to help pay for Alice to attend college.[6] Living under Jim Crow laws, Walker's parents resisted landlords who expected the children of black sharecroppers to work in the fields at a young age. A white plantation owner said to her that black people had 'no need for education'. Minnie Lou Walker, according to her daughter, replied 'You might have some black children somewhere, but they don't live in this house. Don't you ever come around here again talking about how my children don't need to learn how to read and write.' Her mother enrolled Alice in first grade when the girl was four years old.[7]",
+	  image: "images/alicewalker.png",
+	  lifespan: {
+	    birth: "January 26, 1944",
+	    death: "-"
+ 	 	}
+	});
+
+
+
+//on this line, I call the mainWrapper elem from the HTML and set it to the output variable.
+let output = document.getElementById("mainWrapper");
+
+/*in this forEach loop, I am taking all of the objects in the array and placing their elements dynamically so that for each time the output rolls through (however many objects there are) all of the elements will print to the HTML the way I would like.
+
+The one thing to note here is that we are creating dynamic IDs that are assigned to each div based on the order created as it runs through the for loop.
+*/
+	famousFolks.forEach((item, index) => {
+	output.innerHTML += `<div class="person__container" id="${index}">
+	 <img src="${item.image}">
+	 	<div>
+		 	<h2>${item.title}: ${item.name}</h2>
+		 	<section id="bio--${index}">${item.bio}</section>
+			<footer>Birth: ${item.lifespan.birth} | Death: ${item.lifespan.death} </footer>
+		</div>
+	</div>`;
+
+});
+
+// here I have declared all the variables I will need access to in every function I create.
+var personContainer = document.getElementsByClassName("person__container");
+let bioArea = document.getElementById("bio");
+let textInput = document.getElementById("textObj");
+//this variable is created on the global scope so that I can access it in all the variables and so that its value can change each time I call it.
+var personId = "";
+
+// in this for loop, I am creating the event listener that is waiting for a click to add a border to the clicked div and place the focus on the text box
+for (var i = 0; i < personContainer.length; i++) {
+  personContainer[i].addEventListener("click", function (event) {
+    // I set the person ID to target the entire div. Using event.target.id would look at every element within the div individually and we don't want that for this purpose.
+    	personId = event.currentTarget.id;
+    	//because my target was a div, I can target it for addng the border.
+    	let getWhole = document.getElementById(personId);
+    	//here is where I target the div so when I click it, I will get a border
+		getWhole.classList.toggle("border");
+
+		//here I tell the event that after it has added the border to place the cursor (or focus) on the text box.
+		textInput.focus();
+  		});
+
+};
+
+	//this is another event listener that is focusing on the keyup and running a function that changes the text inside of the elements that have the ID specified in the getelementbyid declaration.
+	textInput.addEventListener("keyup", () => {
+		//because I made personId available globally and it was redefined in the function above, I am able to access that value here in the element ID so that it ONLY replaces the bio text inside of the clicked div SOOOOO COOL!!!
+		document.getElementById(`bio--${personId}`).innerText = textInput.value;
+
+		//this code clears the typed text when the enter button is pressed, but does not refresh or submit the page so that the changes made remain.
+		if (event.keyCode == 13) {
+		textInput.value = "";
+	}
+	
+});  
+
